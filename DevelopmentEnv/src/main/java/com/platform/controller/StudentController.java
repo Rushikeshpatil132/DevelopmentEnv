7cce16e476617666b61e6f2b1c38472cf5195cde
ruhikeshpatil132 <rushikeshpatil132@outlook.com>
2020-04-04 17:02:00 +0530

    student controller modified

--- DevelopmentEnv/src/main/java/com/platform/controller/StudentController.java
+++ DevelopmentEnv/src/main/java/com/platform/controller/StudentController.java
@@ -1,8 +1,42 @@
 package com.platform.controller;
 
-import org.springframework.stereotype.Controller;
 
-@Controller
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.platform.entity.Student;
+import com.platform.service.StudentService;
+
+@RestController
 public class StudentController {
 
+	@Autowired
+	private StudentService studentService;
+	
+	public String deleteController() {
+		return "delete";
+	}
+
+//	{"id":"101","sName":"ushi","sAddress":"Latur","contactNo":"9545"}
+	@RequestMapping(value = "/sortbyname/{student}")
+	public Student sortbyName(@RequestBody Student s1) {
+		System.out.println("called methodad");
+		/*
+		 * for (Student student : s1) { System.out.println(student); }
+		 */
+		return null;
+	}
+	
+	@RequestMapping(value = "/hello/{id}",produces ="text/html",method = RequestMethod.GET)
+	public void testStudent(@PathVariable("id") int id) {
+		
+		System.out.println("hii hello param" + id);
+	
+		int result =studentService.findResult(id);
+		System.out.println(result);
+	}
 }
